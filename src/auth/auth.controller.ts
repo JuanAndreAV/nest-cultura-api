ea70114ff94363d688c86c272d0d7c329b691f94
Juan Andres Alvarez <juanandres1029@gmail.com>
2026-01-03 07:50:15 -0500

    Add login and register DTOS

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,20 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { RegisterDto } from './dto/register.dto';
+import { loginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  create(@Body() registerDto: RegisterDto) {
+    return this.authService.create(registerDto);
   }
 
   @Get()
   findAll() {
-    return this.authService.findAll();
+    return "holi";
   }
 
   @Get(':id')
@@ -22,10 +22,10 @@ export class AuthController {
     return this.authService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsEnum, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class RegisterDto {
+    @IsEmail({}, { message: 'El correo electrónico no es válido' })
+  email: string;
+
+  @IsString()
+  @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+  password: string;
+
+  @IsString({message: "El nombre completo es obligatorio"})
+    @MinLength(10, { message: 'El nombre completo debe tener al menos 10 caracteres' })
+    @MaxLength(50, { message: 'El nombre completo es muy largo' })
+    fullName: string;
+
+  @IsOptional()
+  @IsEnum(['admin', 'profesor','estudiante'], { message: 'Rol no válido' })
+  role?: string;
+
+}
