a603f136b82be1739c5793a110c82265d2c67516
Juan Andres Alvarez <juanandres1029@gmail.com>
2026-04-11 09:03:39 -0500

    Change port into app listen

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,8 @@ async function bootstrap() {
       
     })
   )
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 8080;
+  await app.listen(port, '0.0.0.0');
+  
 }
 bootstrap();
